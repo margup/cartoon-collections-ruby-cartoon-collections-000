98a5d5ad0a4666c831e5d470e59710353bcc6b10
bright-lifecycle-9875 <mairgup@icloud.com>
2020-10-07 02:45:20 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,29 +1,37 @@
-def roll_call_dwarves(array)# code an argument here
-index = 0 
-array.each_with_index {|name, index| puts "#{index + 1}. #{name} "}
-  # Your code here
-end
-
-def summon_captain_planet(array)# code an argument here
-    planeteer_calls = []
-  array.collect do |calls|
-    planeteer_calls << "#{calls.capitalize}!"
-  end
-  planeteer_calls
-  # Your code here
-end
-
-def long_planeteer_calls(array)# code an argument here
-     array.any? do |word|
-    word.length > 4
-end
-  # Your code here
-end
-
-
-def find_the_cheese(array)
-  array.find do |cheese|
-    cheese == "cheddar" || cheese == "gouda" || cheese == "camembert"
+def roll_call_dwarves(array)
+  index = 0 
+  array.each_with_index do |(call, index), num|
+    puts "#{index + 1}. #{call}"
+    
   end
 end
+
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map { |call| call.capitalize + '!' }
+  
+end
+
+def long_planeteer_calls(planeteer_calls)
+  planeteer_calls.any? { |call| call.length > 4 }
+end
+
+def find_the_cheese(potentially_cheesy_items)
+  cheeses = ["gouda", "cheddar","camembert"]
+
+  potentially_cheesy_items.find do |maybe_cheese|
+    cheeses.include?(maybe_cheese)
+  end
+end
+
+def word_start_with_B(name)
+  new_array = []
+  
+  name.each do |word|
+    if word[0] == "b" 
+       new_array << word 
+    end
+    
+  
+  end   
+new_array 
 
